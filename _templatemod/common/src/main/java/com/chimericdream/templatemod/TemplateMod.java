99dd4c5dd0b1497656e7ee92fee83a8273e4f10d
Bill Parrott <bill@chimericdream.com>
2024-11-01 22:54:41 -0500

    feat: add template mod

---
+++ _templatemod/common/src/main/java/com/chimericdream/templatemod/TemplateMod.java
@@ -0,0 +1,9 @@
+package com.chimericdream.templatemod;
+
+public final class TemplateMod {
+    public static final String MOD_ID = "templatemod";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ _templatemod/neoforge/src/main/java/com/chimericdream/templatemod/neoforge/TemplateModNeoForge.java
@@ -0,0 +1,12 @@
+package com.chimericdream.templatemod.neoforge;
+
+import com.chimericdream.templatemod.TemplateMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(TemplateMod.MOD_ID)
+public final class TemplateModNeoForge {
+    public TemplateModNeoForge() {
+        // Run our common setup.
+        TemplateMod.init();
+    }
+}
