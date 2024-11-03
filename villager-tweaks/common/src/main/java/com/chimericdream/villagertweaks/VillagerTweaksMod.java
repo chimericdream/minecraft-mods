42187286322692894e91e031e46a507650746c7c
Bill Parrott <bill@chimericdream.com>
2024-11-02 19:52:32 -0500

    feat: add skeleton for villager tweaks

---
+++ villager-tweaks/common/src/main/java/com/chimericdream/villagertweaks/VillagerTweaksMod.java
@@ -0,0 +1,9 @@
+package com.chimericdream.villagertweaks;
+
+public final class VillagerTweaksMod {
+    public static final String MOD_ID = "villagertweaks";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ villager-tweaks/neoforge/src/main/java/com/chimericdream/villagertweaks/neoforge/VillagerTweaksNeoForge.java
@@ -0,0 +1,12 @@
+package com.chimericdream.villagertweaks.neoforge;
+
+import com.chimericdream.villagertweaks.VillagerTweaksMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(VillagerTweaksMod.MOD_ID)
+public final class VillagerTweaksNeoForge {
+    public VillagerTweaksNeoForge() {
+        // Run our common setup.
+        VillagerTweaksMod.init();
+    }
+}
