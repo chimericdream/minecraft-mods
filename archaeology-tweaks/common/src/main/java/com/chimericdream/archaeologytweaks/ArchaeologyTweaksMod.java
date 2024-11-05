56766dbd5a6eea83e10cc47a2da6d22d8895f3c3
Bill Parrott <bill@chimericdream.com>
2024-11-05 09:05:35 -0600

    chore: skeleton for archaeology tweaks

---
+++ archaeology-tweaks/common/src/main/java/com/chimericdream/archaeologytweaks/ArchaeologyTweaksMod.java
@@ -0,0 +1,9 @@
+package com.chimericdream.archaeologytweaks;
+
+public final class ArchaeologyTweaksMod {
+    public static final String MOD_ID = "archaeologytweaks";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ archaeology-tweaks/neoforge/src/main/java/com/chimericdream/archaeologytweaks/neoforge/ArchaeologyTweaksNeoForge.java
@@ -0,0 +1,12 @@
+package com.chimericdream.archaeologytweaks.neoforge;
+
+import com.chimericdream.archaeologytweaks.ArchaeologyTweaksMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ArchaeologyTweaksMod.MOD_ID)
+public final class ArchaeologyTweaksNeoForge {
+    public ArchaeologyTweaksNeoForge() {
+        // Run our common setup.
+        ArchaeologyTweaksMod.init();
+    }
+}
