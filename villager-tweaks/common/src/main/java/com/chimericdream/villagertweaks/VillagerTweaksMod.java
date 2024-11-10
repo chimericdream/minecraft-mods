c1592fdd473abc5f1c4ea7399474b953025a66e2
Bill Parrott <bill@chimericdream.com>
2024-11-09 19:37:05 -0600

    feat: update villager tweaks to use chimericlib

--- villager-tweaks/common/src/main/java/com/chimericdream/villagertweaks/VillagerTweaksMod.java
+++ villager-tweaks/common/src/main/java/com/chimericdream/villagertweaks/VillagerTweaksMod.java
@@ -1,6 +1,7 @@
 package com.chimericdream.villagertweaks;
 
-import com.chimericdream.villagertweaks.registry.ModRegistries;
+import com.chimericdream.lib.registries.ModRegistryHelper;
+import com.chimericdream.villagertweaks.item.ModItems;
 import com.google.common.base.Suppliers;
 import dev.architectury.registry.registries.RegistrarManager;
 import org.slf4j.Logger;
@@ -12,9 +13,12 @@ public class VillagerTweaksMod {
     public static Supplier<RegistrarManager> MANAGER;
     public static final Logger LOGGER = LoggerFactory.getLogger(ModInfo.MOD_ID);
 
+    public static final ModRegistryHelper REGISTRY_HELPER = new ModRegistryHelper(ModInfo.MOD_ID, LOGGER);
+
     public static void init() {
         MANAGER = Suppliers.memoize(() -> RegistrarManager.get(ModInfo.MOD_ID));
 
-        ModRegistries.init();
+        REGISTRY_HELPER.init();
+        ModItems.init();
     }
 }
--- villager-tweaks/common/src/main/java/com/chimericdream/villagertweaks/item/ModItems.java
+++ villager-tweaks/common/src/main/java/com/chimericdream/villagertweaks/item/ModItems.java
@@ -4,12 +4,12 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 
-import static com.chimericdream.villagertweaks.registry.ModRegistries.registerItem;
+import static com.chimericdream.villagertweaks.VillagerTweaksMod.REGISTRY_HELPER;
 
 public class ModItems {
     private static final Item.Settings DEFAULT_SETTINGS = new Item.Settings().maxCount(1).arch$tab(ItemGroups.FUNCTIONAL);
 
-    public static final RegistrySupplier<Item> BAGGED_VILLAGER_ITEM = registerItem(BaggedVillagerItem.ITEM_ID, () -> new BaggedVillagerItem(DEFAULT_SETTINGS));
+    public static final RegistrySupplier<Item> BAGGED_VILLAGER_ITEM = REGISTRY_HELPER.registerItem(BaggedVillagerItem.ITEM_ID, () -> new BaggedVillagerItem(DEFAULT_SETTINGS));
 
     public static void init() {
     }
