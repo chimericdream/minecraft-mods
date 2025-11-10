c6670e05549370de5285754db2e9d05463117f48
Bill Parrott <bill@chimericdream.com>
2025-11-09 21:57:01 -0600

    feat: only release villagers from a bag when it's safe

--- villager-tweaks/common/src/main/java/com/chimericdream/villagertweaks/item/BaggedVillagerItem.java
+++ villager-tweaks/common/src/main/java/com/chimericdream/villagertweaks/item/BaggedVillagerItem.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.Items;
 import net.minecraft.storage.NbtReadView;
 import net.minecraft.storage.ReadView;
@@ -15,7 +16,11 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.ErrorReporter;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+
+import static net.minecraft.block.Block.sideCoversSmallSquare;
 
 public class BaggedVillagerItem extends Item {
     public static final Identifier ITEM_ID = Identifier.of(ModInfo.MOD_ID, "bagged_villager");
@@ -24,7 +29,29 @@ public class BaggedVillagerItem extends Item {
         super(settings);
     }
 
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+
+        if (context.getSide() != Direction.UP || !sideCoversSmallSquare(world, context.getBlockPos(), Direction.UP)) {
+            return ActionResult.FAIL;
+        }
+
+        if (!(world.getBlockState(context.getBlockPos().up(1)).isAir() && world.getBlockState(context.getBlockPos().up(2)).isAir())) {
+            return ActionResult.FAIL;
+        }
+
+        return spawnVillager(context.getWorld(), context.getBlockPos().up(), context.getPlayer(), context.getHand());
+    }
+
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        if (!user.isOnGround()) {
+            return ActionResult.PASS;
+        }
+
+        return spawnVillager(world, user.getBlockPos(), user, hand);
+    }
+
+    private ActionResult spawnVillager(World world, BlockPos pos, PlayerEntity user, Hand hand) {
         VillagerEntity villager = EntityType.VILLAGER.create(world, SpawnReason.BUCKET);
         assert villager != null;
 
@@ -35,7 +62,7 @@ public class BaggedVillagerItem extends Item {
 
             villager.setCustomName(user.getStackInHand(hand).get(DataComponentTypes.CUSTOM_NAME));
             villager.readCustomData(view);
-            villager.refreshPositionAndAngles(user.getBlockPos(), 0, 0);
+            villager.refreshPositionAndAngles(pos, 0, 0);
 
             world.spawnEntity(villager);
 
