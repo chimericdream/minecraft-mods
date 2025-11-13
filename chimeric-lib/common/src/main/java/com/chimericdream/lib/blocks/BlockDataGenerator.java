32b248b94220e38fb5f622ec54acf7b27c15fbd7
Bill Parrott <bill@chimericdream.com>
2025-11-13 17:07:44 -0600

    feat!: bump chimericlib to 4.0.0

--- chimeric-lib/common/src/main/java/com/chimericdream/lib/blocks/BlockDataGenerator.java
+++ chimeric-lib/common/src/main/java/com/chimericdream/lib/blocks/BlockDataGenerator.java
@@ -1,16 +1,28 @@
 package com.chimericdream.lib.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.data.BlockStateModelGenerator;
 import net.minecraft.client.data.ItemModelGenerator;
 import net.minecraft.data.loottable.BlockLootTableGenerator;
 import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.tag.ProvidedTagBuilder;
+import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.function.Function;
 
 public interface BlockDataGenerator {
-    default void configureRecipes(RecipeExporter exporter) {
+    default void configureRecipes(RegistryWrapper.WrapperLookup registryLookup, RecipeExporter exporter) {
     }
 
-    default void configureBlockLootTables(RegistryWrapper.WrapperLookup registryLookup, BlockLootTableGenerator generator) {
+    default void configureBlockTags(RegistryWrapper.WrapperLookup registryLookup, Function<TagKey<Block>, ProvidedTagBuilder<Block, Block>> getBuilder) {
+    }
+
+    default void configureItemTags(RegistryWrapper.WrapperLookup registryLookup, Function<TagKey<Item>, ProvidedTagBuilder<Item, Item>> getBuilder) {
+    }
+
+    default void configureBlockLootTables(BlockLootTableGenerator generator) {
     }
 
     default void configureBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
