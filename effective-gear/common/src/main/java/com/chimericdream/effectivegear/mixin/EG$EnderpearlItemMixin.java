9aa7e402cbb883215de1bcf17723edf871de0d1b
Bill Parrott <bill@chimericdream.com>
2026-08-16 18:10:03 -0500

    feat(effective-gear): starting work on ender pearl trim (WIP)

---
+++ effective-gear/common/src/main/java/com/chimericdream/effectivegear/mixin/EG$EnderpearlItemMixin.java
@@ -0,0 +1,25 @@
+package com.chimericdream.effectivegear.mixin;
+
+import com.chimericdream.effectivegear.ModInfo;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.EnderpearlItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.equipment.trim.TrimMaterial;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(EnderpearlItem.class)
+public class EG$EnderpearlItemMixin {
+    @Unique
+    private static ResourceKey<TrimMaterial> ENDER_PEARL_TRIM_ID = ResourceKey.create(Registries.TRIM_MATERIAL, Identifier.fromNamespaceAndPath(ModInfo.MOD_ID, "ender_pearl"));
+
+    @ModifyVariable(method = "<init>(Lnet/minecraft/world/item/Item$Properties;)V", at = @At("HEAD"), ordinal = 1)
+    private static Item.Properties eg$addTrimMaterialComponent(Item.Properties properties) {
+        return properties.delayedHolderComponent(DataComponents.PROVIDES_TRIM_MATERIAL, ENDER_PEARL_TRIM_ID);
+    }
+}
