158e8506baeb4220dae5e24937de65a04c92c247
Bill Parrott <bill@chimericdream.com>
2026-08-17 00:48:10 -0500

    chore(effective-gear): minor tweak

--- effective-gear/common/src/main/java/com/chimericdream/effectivegear/mixin/EG$EnderpearlItemMixin.java
+++ effective-gear/common/src/main/java/com/chimericdream/effectivegear/mixin/EG$EnderpearlItemMixin.java
@@ -17,7 +17,7 @@ public class EG$EnderpearlItemMixin {
     @Unique
     private static ResourceKey<TrimMaterial> ENDER_PEARL_TRIM_ID = ResourceKey.create(Registries.TRIM_MATERIAL, Identifier.fromNamespaceAndPath(ModInfo.MOD_ID, "ender_pearl"));
 
-    @ModifyVariable(method = "<init>(Lnet/minecraft/world/item/Item$Properties;)V", at = @At("HEAD"), ordinal = 0)
+    @ModifyVariable(method = "<init>(Lnet/minecraft/world/item/Item$Properties;)V", at = @At("HEAD"), argsOnly = true, name = "properties")
     private static Item.Properties eg$addTrimMaterialComponent(Item.Properties properties) {
         return properties.trimMaterial(ENDER_PEARL_TRIM_ID);
     }
