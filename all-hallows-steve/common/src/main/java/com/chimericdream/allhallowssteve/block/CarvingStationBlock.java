46f4d0a7ebb9aadf042b8c8b933dbcde3dcc52e6
Bill Parrott <bill@chimericdream.com>
2026-09-09 22:20:10 -0500

    feat(all-hallows-steve): update carving station textures

--- all-hallows-steve/common/src/main/java/com/chimericdream/allhallowssteve/block/CarvingStationBlock.java
+++ all-hallows-steve/common/src/main/java/com/chimericdream/allhallowssteve/block/CarvingStationBlock.java
@@ -11,22 +11,31 @@ import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
 
 import static com.chimericdream.allhallowssteve.AllHallowsSteveMod.REGISTRY_HELPER;
 
 public class CarvingStationBlock extends BaseEntityBlock {
     public static final Identifier BLOCK_ID = Identifier.fromNamespaceAndPath(ModInfo.MOD_ID, "carving_station");
     public static final MapCodec<CarvingStationBlock> CODEC = simpleCodec(CarvingStationBlock::create);
+    public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
 
     static CarvingStationBlock create(BlockBehaviour.Properties settings) {
         return new CarvingStationBlock() {
@@ -35,6 +44,8 @@ public class CarvingStationBlock extends BaseEntityBlock {
 
     public CarvingStationBlock() {
         super(BlockBehaviour.Properties.ofFullCopy(Blocks.CRAFTING_TABLE).setId(REGISTRY_HELPER.makeBlockRegistryKey(BLOCK_ID)));
+
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     public @NotNull MapCodec<CarvingStationBlock> codec() {
@@ -42,17 +53,37 @@ public class CarvingStationBlock extends BaseEntityBlock {
     }
 
     @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        return this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected @NotNull BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    protected @NotNull BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(@NonNull BlockPos pos, @NonNull BlockState state) {
         return new CarvingStationBlockEntity(pos, state);
     }
 
     @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level world, BlockState state, BlockEntityType<T> type) {
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NonNull Level world, @NonNull BlockState state, @NonNull BlockEntityType<T> type) {
         return createTickerHelper(type, ModBlocks.CARVING_STATION_BLOCK_ENTITY.get(), CarvingStationBlockEntity::tick);
     }
 
     @Override
-    protected @NotNull InteractionResult useWithoutItem(BlockState state, Level world, BlockPos pos, Player player, BlockHitResult hit) {
+    protected @NotNull InteractionResult useWithoutItem(@NonNull BlockState state, Level world, @NonNull BlockPos pos, @NonNull Player player, @NonNull BlockHitResult hit) {
         if (world.isClientSide()) {
             return InteractionResult.SUCCESS;
         }
@@ -66,7 +97,7 @@ public class CarvingStationBlock extends BaseEntityBlock {
     }
 
     @Override
-    protected void affectNeighborsAfterRemoval(BlockState state, ServerLevel world, BlockPos pos, boolean moved) {
+    protected void affectNeighborsAfterRemoval(@NonNull BlockState state, ServerLevel world, @NonNull BlockPos pos, boolean moved) {
         BlockEntity blockEntity = world.getBlockEntity(pos);
         if (blockEntity instanceof CarvingStationBlockEntity) {
             Containers.dropContents(world, pos, (CarvingStationBlockEntity) blockEntity);
@@ -76,12 +107,12 @@ public class CarvingStationBlock extends BaseEntityBlock {
     }
 
     @Override
-    public boolean hasAnalogOutputSignal(BlockState state) {
+    public boolean hasAnalogOutputSignal(@NonNull BlockState state) {
         return true;
     }
 
     @Override
-    public int getAnalogOutputSignal(BlockState state, Level world, BlockPos pos, Direction direction) {
+    public int getAnalogOutputSignal(@NonNull BlockState state, Level world, @NonNull BlockPos pos, @NonNull Direction direction) {
         return AbstractContainerMenu.getRedstoneSignalFromBlockEntity(world.getBlockEntity(pos));
     }
 }
